7e42e2c4525a53afa9071def1bdf57b2c8f288cb
LeeHero0803 <leehenry1024@qq.com>
2026-03-21 13:06:04 +0800

    style: 大幅修改样式细节：首页卡片、正文标题、二级标题

--- astro.config.mjs
+++ astro.config.mjs
@@ -78,8 +78,7 @@ export default defineConfig({
 				borderRadius: "0rem",
 				borderColor: "none",
 				codeFontSize: "0.875rem",
-				codeFontFamily:
-					"'JetBrains Mono Variable', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+				codeFontFamily: "var(--font-mono)",
 				codeLineHeight: "1.5rem",
 				frames: {
 					editorBackground: "var(--codeblock-bg)",
